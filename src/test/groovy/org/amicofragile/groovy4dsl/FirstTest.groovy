8ce3c4892a901d378dc6b2cebbe94ca458123730
Pietro Martinelli <amicofragile@gmail.com>
2013-08-15 23:46:39 +0200

    First Groovy test - To be removed

---
+++ src/test/groovy/org/amicofragile/groovy4dsl/FirstTest.groovy
@@ -0,0 +1,14 @@
+package org.amicofragile.groovy4dsl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+class FirstTest {
+
+	@Test
+	public void test() {
+		assertTrue(1 == 1)
+	}
+
+}
